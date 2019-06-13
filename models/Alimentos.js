9551226f8e011ba3e2a998ce783a335d3fe95955
Elida <elidacpsouza@gmail.com>
2019-06-13 11:02:07 -0300

    alimentos

--- models/Alimentos.js
+++ models/Alimentos.js
@@ -1,21 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const AlimentosSchema = new mongoose.Schema({
+const AlimentosSchema = new mongoose.Schema(
+  {
     descricao: String,
     calorias: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     cho: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     quantidade: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0
     },
     unidade: {
-        descricao: String,
-        id : Number
+      descricao: String,
+      id: Number
     }
-})
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Alimentos", AlimentosSchema);
