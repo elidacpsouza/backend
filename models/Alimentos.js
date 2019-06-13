428e86444d5b59dd352562b84ebf578d698b90dc
Elida <elidacpsouza@gmail.com>
2019-06-13 10:55:36 -0300

    sincronia

---
+++ models/Alimentos.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const AlimentosSchema = new mongoose.Schema({
+    descricao: String,
+    calorias: {
+        type: Number,
+        default: 0
+    },
+    cho: {
+        type: Number,
+        default: 0
+    },
+    quantidade: {
+        type: Number,
+        default: 0
+    },
+    unidade: {
+        descricao: String,
+        id : Number
+    }
+})
